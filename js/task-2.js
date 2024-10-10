668956774dd805607a4accc2042f12d7f00c7146
Salih Enes Alıcı <enesalici.tr@gmail.com>
2024-10-10 17:29:53 +0300

    added homework

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+class Storage {
+  #items;
+
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(removeItem) {
+    this.#items = this.#items.filter((item) => item !== removeItem);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
+storage.addItem("Droid");
+console.log(storage.getItems());
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
